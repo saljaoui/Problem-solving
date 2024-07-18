8921fed03e19e4d9d9db90e16e0d58f5c989698d
soufian <161062883+saljaoui@users.noreply.github.com>
2024-07-18 10:17:27 +0100

    Update timeConversion.go

--- timeConversion.go
+++ timeConversion.go
@@ -10,7 +10,7 @@ import (
 
 /*
  * Complete the 'timeConversion' hhifunction below.
- * soufian 1 w
+ * soufian
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
