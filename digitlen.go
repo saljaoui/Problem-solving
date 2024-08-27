4119e22b602d97637847396c9dae5afafcfabaae
soufian <161062883+saljaoui@users.noreply.github.com>
2024-08-27 11:32:30 +0100

    Update digitlen.go

--- digitlen.go
+++ digitlen.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
 )
 
 func DigitLen(n, base int) int {
-	if base < 2 || base > 32 {
+	if base < 2 || base > 36 {
 		return -1
 	}
-	var r int
 	if n < 0 {
-		n = n * -1
+		n = -n
 	}
-	for n > 0 {
+	cont := 0
+	for n != 0 {
+		cont++
 		n = n / base
-		r++
 	}
-	return r
+	return cont
+
 }
 
 func main() {
-	fmt.Println(DigitLen(100, 10))
-	fmt.Println(DigitLen(100, 2))
-	fmt.Println(DigitLen(-100, 16))
-	fmt.Println(DigitLen(100, -1))
+	table := [][]int{
+		{10, 2},
+		{10, 3},
+		{10, 4},
+		{10, 5},
+		{10, 6},
+		{100, -1},
+		{100, 10},
+		{-100, 10},
+		{-45, -1},
+		{-100, 2},
+		{456, 2},
+		{-233, 15},
+		{-100, 8},
+		{-432, 16},
+		{100, 36},
+		{-100, 36},
+		{100, 37},
+		{-100, 37},
+		{100, 1},
+		{-100, 1},
+	}
+
+	for _, arg := range table {
+		challenge.Function("DigitLen", DigitLen, solutions.DigitLen, arg[0], arg[1])
+	}
 }
+
