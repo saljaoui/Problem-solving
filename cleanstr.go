6c6ba13fa252b377df449a6fa727be7dd8f02216
soufian <161062883+saljaoui@users.noreply.github.com>
2024-08-27 15:43:40 +0100

    Create cleanstr.go

---
+++ cleanstr.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+	str := os.Args[1]
+	fmt.Println(str)
+    var res string
+    for i, s := range str {
+
+        if s != ' ' {
+            res += string(s)
+        } else if len(str)-1 > i && s == ' ' && str[i+1] != ' ' && res != "" {
+            res += " "
+        }
+    }
+    fmt.Println(res)
+}
+
